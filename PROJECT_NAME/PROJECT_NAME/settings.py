3ba369dcec2d6bdb6609bda30397c17acf05874b
VeLUlin <kobaryo1112@gmail.com>
2022-03-22 11:01:36 +0900

    ログイン機能wo

--- PROJECT_NAME/PROJECT_NAME/settings.py
+++ PROJECT_NAME/PROJECT_NAME/settings.py
@@ -118,9 +118,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [str(BASE_DIR / 'PROJECT_NAME/static')]
+
+
+
+MEDIA_URL = 'medi/'
+MEDIA_ROOT = BASE_DIR / 'PROJECT_NAME/media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'hello/login.html'
--- PROJECT_NAME/PROJECT_NAME/urls.py
+++ PROJECT_NAME/PROJECT_NAME/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
     path('newyear/', include("newyear.urls")),
     path('tasks/', include("tasks.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
