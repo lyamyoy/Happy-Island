fc9881c519c9b47a6e8e15e946f7a04c6b72b958
ka300-g <kazaore@icloud.com>
2022-03-26 08:29:02 +0900

    demo-day

--- PROJECT_NAME/rooms/views.py
+++ PROJECT_NAME/rooms/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import CreateView, TemplateView
 
 from .models import Room, Post
 # from cs50 import SQL
 
 # db = SQL("sqlite:///db.sqlite3")
 
+"""
+class PostCreateView(CreateView):
+    template_name = 'post_create.html' # PostCreateViewに紐づくテンプレートを表している
+    form_class = PostForm # post_create.html内部でPostFormを用いた入力フォームを作成することができる
+    success_url = reverse_lazy('post:post_create_complete') # 入力フォームを投稿した後に、遷移する url を指定
+
+
+class PostCreateCompleteView(TemplateView):
+    template_name = 'post_create_complete.html'
+"""
+
+
 # Create your views here.
 def index(request):
-  return render(request, "rooms/index.html", {
-    "rooms": Room.objects.all()
+  return redirect("../hello/login", {
+    # "rooms": Room.objects.all()
   })
 
 
