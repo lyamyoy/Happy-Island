d6d9dadc3e05d0aa1f819803a8954d101a15403d
lyamyoy <57428385+lyamyoy@users.noreply.github.com>
2022-03-25 10:14:09 +0900

    Revert "Yes we can"

--- PROJECT_NAME/newyear/apps.py
+++ PROJECT_NAME/newyear/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RoomsConfig(AppConfig):
+class NewyearConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rooms'
+    name = 'newyear'
